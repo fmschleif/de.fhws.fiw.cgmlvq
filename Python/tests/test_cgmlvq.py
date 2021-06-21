386efe487be8038ff0081aaae0a067ab741a687b
Matthias Nunn <matthiasnunn@gmx.de>
2021-06-21 11:21:11 +0200

    Reanamed test variables

--- Python/tests/test_cgmlvq.py
+++ Python/tests/test_cgmlvq.py
@@ -61,9 +61,9 @@ class Test_CGMLVQ( unittest.TestCase ):
 
         cgmlvq = CGMLVQ()
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
-        costf, crout, marg, score = cgmlvq._CGMLVQ__compute_costs( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, 0 )
+        costf, crout, marg, score = cgmlvq._CGMLVQ__compute_costs( self.X_train, self.y_train, w, np.unique(self.y_train), omega, 0 )
 
         np.testing.assert_array_almost_equal( costf, self.load_data('test_compute_costs_costf.csv')[0] )
         np.testing.assert_array_almost_equal( crout, self.load_data('test_compute_costs_crout.csv')    )
@@ -73,9 +73,9 @@ class Test_CGMLVQ( unittest.TestCase ):
 
         cgmlvq.set_params( mu=1 )
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
-        costf, crout, marg, score = cgmlvq._CGMLVQ__compute_costs( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, 1 )
+        costf, crout, marg, score = cgmlvq._CGMLVQ__compute_costs( self.X_train, self.y_train, w, np.unique(self.y_train), omega, 1 )
 
         np.testing.assert_array_almost_equal( costf, self.load_data('test_compute_costs_mu1_costf.csv')[0] )
         np.testing.assert_array_almost_equal( crout, self.load_data('test_compute_costs_mu1_crout.csv')    )
@@ -87,54 +87,54 @@ class Test_CGMLVQ( unittest.TestCase ):
 
         cgmlvq = CGMLVQ()
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
-        omat = omi / np.sqrt(sum(sum(omi*omi)))
+        omat = omega / np.sqrt(sum(sum(omega*omega)))
 
-        D = cgmlvq._CGMLVQ__compute_euclid( self.X_train[0,:], proti[0,:], omat )
+        d = cgmlvq._CGMLVQ__compute_euclid( self.X_train[0,:], w[0,:], omat )
 
-        np.testing.assert_array_almost_equal( D, self.load_data('test_euclid_D.csv') )
+        np.testing.assert_array_almost_equal( d, self.load_data('test_euclid_D.csv') )
 
 
     def test_do_batchstep( self ):
 
         cgmlvq = CGMLVQ()
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
         etam, etap, _, _, _ = cgmlvq._CGMLVQ__get_parameters( self.X_train )
 
 
         cgmlvq.set_params( mode=0, mu=1 )
 
-        prot, omat = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, etap, etam )
+        wi, omegai = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, w, np.unique(self.y_train), omega, etap, etam )
 
-        np.testing.assert_array_almost_equal( prot, self.load_data('test_do_batchstep_mode0_prot.csv') )
-        np.testing.assert_array_almost_equal( omat, self.load_data('test_do_batchstep_mode0_omat.csv') )
+        np.testing.assert_array_almost_equal( wi, self.load_data('test_do_batchstep_mode0_prot.csv') )
+        np.testing.assert_array_almost_equal( omegai, self.load_data('test_do_batchstep_mode0_omat.csv') )
 
 
         cgmlvq.set_params( mode=1, mu=0 )
 
-        prot, omat = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, etap, etam )
+        wi, omegai = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, w, np.unique(self.y_train), omega, etap, etam )
 
-        np.testing.assert_array_almost_equal( prot, self.load_data('test_do_batchstep_mode1_prot.csv') )
-        np.testing.assert_array_almost_equal( omat, self.load_data('test_do_batchstep_mode1_omat.csv') )
+        np.testing.assert_array_almost_equal( wi, self.load_data('test_do_batchstep_mode1_prot.csv') )
+        np.testing.assert_array_almost_equal( omegai, self.load_data('test_do_batchstep_mode1_omat.csv') )
 
 
         cgmlvq.set_params( mode=2 )
 
-        prot, omat = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, etap, etam )
+        wi, omegai = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, w, np.unique(self.y_train), omega, etap, etam )
 
-        np.testing.assert_array_almost_equal( prot, self.load_data('test_do_batchstep_mode2_prot.csv') )
-        np.testing.assert_array_almost_equal( omat, self.load_data('test_do_batchstep_mode2_omat.csv') )
+        np.testing.assert_array_almost_equal( wi, self.load_data('test_do_batchstep_mode2_prot.csv') )
+        np.testing.assert_array_almost_equal( omegai, self.load_data('test_do_batchstep_mode2_omat.csv') )
 
 
         cgmlvq.set_params( mode=3 )
 
-        prot, omat = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, proti, np.unique(self.y_train), omi, etap, etam )
+        wi, omegai = cgmlvq._CGMLVQ__do_batchstep( self.X_train, self.y_train, w, np.unique(self.y_train), omega, etap, etam )
 
-        np.testing.assert_array_almost_equal( prot, self.load_data('test_do_batchstep_mode3_prot.csv') )
-        np.testing.assert_array_almost_equal( omat, self.load_data('test_do_batchstep_mode3_omat.csv') )
+        np.testing.assert_array_almost_equal( wi, self.load_data('test_do_batchstep_mode3_prot.csv') )
+        np.testing.assert_array_almost_equal( omegai, self.load_data('test_do_batchstep_mode3_omat.csv') )
 
 
     def test_do_fourier( self ):
@@ -150,22 +150,22 @@ class Test_CGMLVQ( unittest.TestCase ):
 
         cgmlvq = CGMLVQ()
 
-        fvec, mf, st = cgmlvq._CGMLVQ__do_zscore( self.X_train )
+        X, mean, std = cgmlvq._CGMLVQ__do_zscore( self.X_train )
 
-        fvec = cgmlvq._CGMLVQ__do_inversezscore( fvec, mf, st )
+        X = cgmlvq._CGMLVQ__do_inversezscore( X, mean, std )
 
-        np.testing.assert_array_almost_equal( fvec, self.load_data('test_do_inversezscore_fvec.csv') )
+        np.testing.assert_array_almost_equal( X, self.load_data('test_do_inversezscore_fvec.csv') )
 
 
     def test_do_zscore( self ):
 
         cgmlvq = CGMLVQ()
 
-        fvec, mf, st = cgmlvq._CGMLVQ__do_zscore( self.X_train )
+        X, mean, std = cgmlvq._CGMLVQ__do_zscore( self.X_train )
 
-        np.testing.assert_array_almost_equal( fvec, self.load_data('test_do_zscore_fvec.csv') )
-        np.testing.assert_array_almost_equal( mf, self.load_data('test_do_zscore_mf.csv') )
-        np.testing.assert_array_almost_equal( st, self.load_data('test_do_zscore_st.csv') )
+        np.testing.assert_array_almost_equal( X, self.load_data('test_do_zscore_fvec.csv') )
+        np.testing.assert_array_almost_equal( mean, self.load_data('test_do_zscore_mf.csv') )
+        np.testing.assert_array_almost_equal( std, self.load_data('test_do_zscore_st.csv') )
 
 
     def test_get_initial( self ):
@@ -173,18 +173,18 @@ class Test_CGMLVQ( unittest.TestCase ):
         cgmlvq = CGMLVQ()
         cgmlvq.set_params( mode=2, rndinit=True )
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
-        np.testing.assert_array_almost_equal( proti, self.load_data('test_set_initial_proti.csv') )
-        np.testing.assert_array_almost_equal( omi, self.load_data('test_set_initial_omi.csv') )
+        np.testing.assert_array_almost_equal( w, self.load_data('test_set_initial_proti.csv') )
+        np.testing.assert_array_almost_equal( omega, self.load_data('test_set_initial_omi.csv') )
 
 
         cgmlvq.set_params( mode=3, rndinit=False )
 
-        proti, omi = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
+        w, omega = cgmlvq._CGMLVQ__get_initial( self.X_train, self.y_train, np.unique(self.y_train) )
 
-        np.testing.assert_array_almost_equal( proti, self.load_data('test_set_initial_mode3_proti.csv') )
-        np.testing.assert_array_almost_equal( omi, self.load_data('test_set_initial_mode3_omi.csv') )
+        np.testing.assert_array_almost_equal( w, self.load_data('test_set_initial_mode3_proti.csv') )
+        np.testing.assert_array_almost_equal( omega, self.load_data('test_set_initial_mode3_omi.csv') )
 
 
     def test_run_single( self ):
